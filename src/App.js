1ded5cfe017482919878dae60d257944dd050bcd
shreyaTiwari29 <shreya.2902tiwari@gmail.com>
2023-12-31 04:42:18 -0800

    ProjectUpdated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calc from './MyComponent/Calc'
+function App() {
+  return (
+    <div className='App'> 
+    <h2>Basic Calculator</h2>
+    <Calc/>
+    </div>
+  );
+}
+
+export default App;
